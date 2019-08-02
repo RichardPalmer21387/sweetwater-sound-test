581b568a0bbeefd714be2caa5ee8e6e84138500b
BEHEMOTH\Richard <yenraven@gmail.com>
2019-08-01 21:44:22 -0500

    Setup initial display of Info in cart item.

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -13,9 +13,19 @@ export interface ICartItem {
     available: number;
 }
 
+function Info(props:ICartItem){
+    return <div className="cart-item-info">
+        <div className="product-name">{props.productName}</div>
+        <div className="description">{props.description}</div>
+        <div className="quantity"><input type="number" value={props.quantity} /></div>
+    </div>
+}
 
 export function CartItem(props:ICartItem){
     return <div className="cart-item">
-
+        <a href={props.url}>
+            <img src={props.image} alt={props.productName} />
+        </a>
+        <Info {...props} />
     </div>
 }
